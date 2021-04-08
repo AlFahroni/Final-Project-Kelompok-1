19554d6afa9da180439c5b4634ba9553b26d75cd
mrafi.sfauzi <mrafi.sfauzi@gmail.com>
2021-04-08 21:03:01 +0700

    menambahkan tugas router vuejs

---
+++ Tugas-13-Component/ComponentSatu.js
@@ -0,0 +1,7 @@
+export const Component1 = {
+    template: `
+        <div class="alert alert-success">
+            <h1>Halaman Satu</h1>            
+        </div>
+    ` 
+}
---
+++ Tugas-13-Component/ComponentTiga.js
@@ -0,0 +1,7 @@
+export const Component3 = {
+    template: `
+        <div class="alert alert-danger">
+            <h1>Halaman Tiga</h1>            
+        </div>
+    `
+}
